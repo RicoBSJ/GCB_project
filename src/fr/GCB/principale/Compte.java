3cd539e9757664025c2c8407b275695c705e3f4f
Bikach <chakchak75@gmail.com>
2017-12-07 22:20:16 +0100

    private variable, create plusieur méthode de controle des saisie utilisateur

--- src/fr/GCB/principale/Compte.java
+++ src/fr/GCB/principale/Compte.java
@@ -3,45 +3,35 @@ package fr.GCB.principale;
 import java.util.Scanner;
 
 public class Compte {
-	public String typeCompte, numeroCompte;
-	public double valeur, taux;
-	public int nbLigneCompta;
+	private String typeCompte, numeroCompte;
+	private double valeur, taux;
+	private int nbLigneCompta;
 	LigneComptable ligne;
 	
 	Scanner lectureClavier = new Scanner(System.in);
 	
 	public void creerCompte(){
-		do { 
-			System.out.print("\nType du compte [Types possibles :" ); 
-			System.out.print("Courant, Joint, Epargne] :");		
-			typeCompte = lectureClavier.next().toLowerCase();
-			typeCompte = typeCompte.replace('é', 'e');
-		 } while (!typeCompte.equalsIgnoreCase("courant") && !typeCompte.equalsIgnoreCase("joint") && !typeCompte.equalsIgnoreCase("epargne"));
+		System.out.print("\nType du compte [Types possibles :" ); 
+		typeCompte = controleType();
 		
-		switch (typeCompte){
-			case "courant" : typeCompte = "Courant";
-			break;
-			case "joint" : typeCompte = "Joint";
-			break;
-			case "epargne" : typeCompte = "Epargne";
-			break;
-	}
-			System.out.print("\nNuméro du compte : ");
-			numeroCompte = lectureClavier.next();
-			System.out.print("Première valeur à créditer : ");
-			valeur = lectureClavier.nextDouble();						
-		 if ( typeCompte.equalsIgnoreCase("epargne")){
+		System.out.print("\nNuméro du compte : ");
+		numeroCompte = lectureClavier.next();
+		
+		System.out.print("Première valeur à créditer : ");
+		valeur = controleValeur();
+		
+		//Affiche uniquement si le compte est de type épargne
+		if ( typeCompte.equalsIgnoreCase("Epargne")){
 			System.out.print("Taux de placement : ");
 			taux = lectureClavier.nextDouble();	
 		 };
-
 	}
 	
 	public void creerLigne(){
 		ligne = new LigneComptable();		
 		ligne.creerLigneComptable();
 		nbLigneCompta++;
-		valeur += ligne.valeur;
+		valeur += ligne.getValeur();
 	}
 	
 	public void afficherCmpte(){
@@ -53,8 +43,10 @@ public class Compte {
 			System.out.println("\nLe compte numero : " + numeroCompte
 					+ "\n- est un compte : " + typeCompte
 					+ "\n- à pour actif " + valeur + " €");
+			
 			//Affiche uniquement si le compte est de type épargne
-			if(typeCompte.equals("epargne"))	System.out.println("\n- à un taux de : " + taux +" %");
+			if(typeCompte.equals("Epargne"))	System.out.println("- taux de placement : " + taux +" %");
+			
 			//Vérifie l'existance d'une ligne comptable et l'affiche
 			if(nbLigneCompta == 1) ligne.afficherLigne();
 								
@@ -63,6 +55,83 @@ public class Compte {
 		}
 		
 	}
+	
+	//Méthodes de controle de saisie
+	private String controleType(){
+		char tmpC;
+		String tmpS = null;
+		do { 
+			System.out.print("(C).courant, (J).joint, (E).épargne] :");		
+			tmpC = lectureClavier.next().toUpperCase().charAt(0);
+			
+	    } while (tmpC != 'C' && tmpC != 'J' && tmpC != 'E');
+		
+		switch (tmpC){
+			case 'C' : tmpS = "Courant";
+			break;
+			case 'J' : tmpS = "Joint";
+			break;
+			case 'E' : tmpS = "Epargne";
+			break;		
+		}
+		
+		return tmpS;
+	}
+	
+	private double controleValeur(){
+		double tmpd;
+		do { 
+			tmpd = lectureClavier.nextDouble();
+			if(tmpd < 0) System.out.print("Rentrez une valeur positive :");
+			
+	    } while (tmpd < 0); 
+		
+		return tmpd;			
+	}
+	
+
+	////guetteur/setteur
+	
+	public String getTypeCompte() {
+		return typeCompte;
+	}
+
+	public void setTypeCompte(String typeCompte) {
+		this.typeCompte = typeCompte;
+	}
+
+	public String getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	public void setNumeroCompte(String numeroCompte) {
+		this.numeroCompte = numeroCompte;
+	}
+
+	public double getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(double valeur) {
+		this.valeur = valeur;
+	}
+
+	public double getTaux() {
+		return taux;
+	}
+
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+
+	public int getNbLigneCompta() {
+		return nbLigneCompta;
+	}
+
+	public void setNbLigneCompta(int nbLigneCompta) {
+		this.nbLigneCompta = nbLigneCompta;
+	}
+
 	
 
 
--- src/fr/GCB/principale/LigneComptable.java
+++ src/fr/GCB/principale/LigneComptable.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class LigneComptable {
 		
-	public double valeur = 0.0d;
-	public String motif, modePaiement, date;
+	private String motif, modePaiement, date;
+	private double valeur = 0.0d;
 	
 	Scanner lectureClavier = new Scanner(System.in);
 	
 	public void creerLigneComptable(){
+		System.out.println("Indiquez le numéro du compte : ");
+		
 		
 		System.out.print("La somme à créditer (+) ou à débiter (-) : ");
 		valeur = lectureClavier.nextInt();
@@ -17,49 +19,97 @@ public class LigneComptable {
 		System.out.print("La date de l'opération (dd/mm/yyyy) : ");
 		date = lectureClavier.next();
 		
-		do{
-			System.out.print("Le motif [Salaire, Loyer, Alimentation, Divers] : ");
-			motif = lectureClavier.next();
+		System.out.print("Le motif ");
+		motif = controleMotif();
 		
-		}while(!motif.equalsIgnoreCase("Salaire") && !motif.equalsIgnoreCase("Loyer") && !motif.equalsIgnoreCase("Alimentation") && !motif.equalsIgnoreCase("Divers"));
-		
-		switch (motif){
-			case "salaire" : motif = "Salaire";
-			break;
-			case "loyer" : motif = "Loyer";
-			break;
-			case "alimentation" : motif = "Alimentation";
-			break;
-			case "divers" : motif = "Divers";
-			break;
-		}
-		
-		do{
-			System.out.print("Le mode de paiement [CB, Chèque, Virement] : ");
-			modePaiement = lectureClavier.next().toLowerCase().replace('è', 'e');
-		
-		}while(!modePaiement.equalsIgnoreCase("cb") && !modePaiement.equalsIgnoreCase("cheque") && !modePaiement.equalsIgnoreCase("virement"));
-		
-		switch (modePaiement){
-		case "cb" : modePaiement = "CB";
-		break;
-		case "cheque" : modePaiement = "Chèque";
-		break;
-		case "virement" : modePaiement = "Virement";
-		break;
-		}
-		
+		System.out.print("Le mode ");
+		modePaiement = controleModePaiement();
 		
 	}
 	
-	public void afficherLigne(){
-		
+	public void afficherLigne(){		
 		System.out.println("\nEn date du : " + date);
 		if(valeur > 0) System.out.println("- Montant créditer : " + valeur + "€");
 		else System.out.println("- Montant débiter : " +valeur+ "€");
 		System.out.println("- Motif : " + motif);
-		System.out.println("- Moyen de paiement : " + modePaiement);	
+		System.out.println("- Mode de paiement : " + modePaiement);	
+	}
+	
+	//Méthodes controle de saisie
+	private String controleMotif(){
+		char tmpC;
+		String tmpS = null;
+		do { 
+			System.out.print("[(S).salaire, (L).loyer, (A).Alimentation, (D).divers] : ");
+			tmpC = lectureClavier.next().toUpperCase().charAt(0);
+			
+	    } while (tmpC != 'S' && tmpC != 'L' && tmpC != 'A' && tmpC != 'D');
+		
+		switch (tmpC){
+			case 'S' : tmpS = "Salaire";
+			break;
+			case 'L' : tmpS = "Loyer";
+			break;
+			case 'A' : tmpS = "Alimentation";
+			break;
+			case 'D' : tmpS = "Divers";
+			break;
+		}
+		return tmpS;
+	}	
+	
+	private String controleModePaiement(){
+		char tmpC;
+		String tmpS = null;
+		do { 
+			System.out.print("[(C).CB, (Q).chèque, (V).virement] : ");	
+			tmpC = lectureClavier.next().toUpperCase().charAt(0);
+			
+	    } while (tmpC != 'C' && tmpC != 'Q' && tmpC != 'V');
+		
+		switch (tmpC){
+			case 'C' : tmpS = "CB";
+			break;
+			case 'Q' : tmpS = "chèque";
+			break;
+			case 'V' : tmpS = "virement";
+			break;		
+		}
+		
+		return tmpS;
 	}
 	
 	
+	// getter/setter
+	public double getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(double valeur) {
+		this.valeur = valeur;
+	}
+
+	public String getMotif() {
+		return motif;
+	}
+
+	public void setMotif(String motif) {
+		this.motif = motif;
+	}
+
+	public String getModePaiement() {
+		return modePaiement;
+	}
+
+	public void setModePaiement(String modePaiement) {
+		this.modePaiement = modePaiement;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
 }
